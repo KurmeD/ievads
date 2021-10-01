53d5a4f90fb862938ae3f238e387d2b5854965f5
KurmeD <54103875+KurmeD@users.noreply.github.com>
2021-10-01 10:03:26 +0300

    ievads ar vardiem un funkcija

--- programma1.py
+++ programma1.py
@@ -5,3 +5,19 @@ skaitlis=5
 vardi="Skola"
 print(skaitlis)
 print(vardi)
+
+va, vs, vd = "Saule", "Vejs", "udens"
+print(va)
+print(vs)
+print(vd)# trīs mainīgie vienā salikumā
+
+m1 = m2 = m3="Viens simbols"
+print( m1 , m2, m3) #parversam 3 mainigos par vienu izvadi
+
+def nosaukumi(name):
+    print("Sveiki", name)
+
+nosaukumi("Daina")
+nosaukumi("Ieva")
+nosaukumi("Kļava")#funkciju definesana
+
